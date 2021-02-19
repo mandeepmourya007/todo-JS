467832146dddf6d7f9effbb6729266ef0cf893cd
mandeep <mandeepmourya007@gmail.com>
2021-02-19 21:24:35 +0530

    done btn done

--- script.js
+++ script.js
@@ -5,12 +5,37 @@ let input = document.querySelector("#add_item");
 let btn = document.querySelector("#add_btn")
 let item_list = document.querySelector("#item_list");
 let list = []
+let item;
+let id = 1;
+
 btn.addEventListener("click", function (e) {
     e.preventDefault();
-    list.push(input.value)
-    let item = document.createElement("p");
-    item.innerText = input.value;
-    item_list.appendChild(item);
+    list.push(input.value);
+    item = document.createElement("p");
+    let edit_btn = document.createElement("BUTTON");
+
+    let delete_btn = document.createElement("BUTTON");
+    edit_btn.textContent = `  edit`;
+    delete_btn.innerText = "done";
+    console.log(item);
+
+    item_list.append(item);
+
+    item.textContent = input.value;
+
+    item.id = id++;
+    item.append(edit_btn);
+    item.appendChild(delete_btn);
+
+    delete_btn.addEventListener("click", function () {
+
+        delete_btn.parentElement.remove();
+
+    })
+    edit_btn.addEventListener("click", function () {
+        console.log("yoyo")
+    })
+
 }
 )
 // btn.addEventListner(
